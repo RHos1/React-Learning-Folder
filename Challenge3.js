458d6b8a1db74ecf84c0d3eff957a93e03d3e088
Rechad Hossaini <109422736+RHos1@users.noreply.github.com>
2025-01-23 18:19:56 +0000

    Update Challenge3.js

--- Challenge3.js
+++ Challenge3.js
@@ -1,4 +1,5 @@
 const root = document.querySelector("#root");
         const header = document.createElement("h1");
+        header.className = "header";
         header.textContent = "Hello, React!";
         root.appendChild(header);
