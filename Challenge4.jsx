e6fdfac51da959c79412343eb8619acbd235a8ac
Rechad Hossaini <109422736+RHos1@users.noreply.github.com>
2025-01-23 18:44:46 +0000

    Update Challenge4.jsx

--- Challenge4.jsx
+++ Challenge4.jsx
@@ -1,5 +1,5 @@
 import {createRoot} from "react-dom/client"
 
-const root = createRoot(document.getElementbyId("root"))
+const root = createRoot(document.getElementById("root"))
 
 root.render(<h1>React is Fun</h1>)
