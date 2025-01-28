990efe8d54544fb8e4a61d80078604117d780203
Rechad Hossaini <109422736+RHos1@users.noreply.github.com>
2025-01-28 17:03:12 +0000

    Create Challenge12a.jsx

---
+++ Challenge12a.jsx
@@ -0,0 +1,42 @@
+import Jokes from "./Jokes.jsx"
+export default function App() {
+    return (
+        <>
+        <Jokes 
+        joke = "I got my daughter a fridge for her birthday."
+        punchline = "I can't wait to see her face light up when she opens it."
+        
+        />
+        
+         <Jokes 
+        joke = "How did the hacker escape the police?"
+        punchline = "He just ransomware!"
+        
+        />
+        
+        
+         <Jokes 
+        joke = "Why don't pirates travel on mountain roads?"
+        punchline = "Scurvy."
+        
+        />
+        
+        
+         <Jokes 
+        joke = "Why do bees stay in the hive in the winter?"
+        punchline = "Swarm."
+        
+        />
+        
+        <Jokes 
+        joke = "What's the best thing about Switzerland?"
+        punchline = "I don't know, but the flag is a big plus!"
+        
+        />
+       
+       
+       
+       
+        </>
+    )
+}
