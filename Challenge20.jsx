d10d4dd9b99b8267d03ab6ee49a74340567586c4
Rechad Hossaini <109422736+RHos1@users.noreply.github.com>
2025-02-10 17:16:08 +0000

    Create Challenge20.jsx

---
+++ Challenge20.jsx
@@ -0,0 +1,22 @@
+import React from "react"
+export default function App() {
+
+  const [myFavoriteThings, change] = React.useState([]);
+  
+  const allFavoriteThings = ["💦🌹", "😺", "💡🫖", "🔥🧤", "🟤🎁", 
+  "🐴", "🍎🥧", "🚪🔔", "🛷🔔", "🥩🍝"]
+  const thingsElements = myFavoriteThings.map(thing => <p key={thing}>{thing}</p>)
+
+  function addFavoriteThing() {
+    // We'll work on this next, nothing to do here yet.
+  }
+  
+  return (
+    <main>
+      <button onClick={addFavoriteThing}>Add item</button>
+      <section aria-live="polite">
+        {thingsElements}
+      </section>
+    </main>
+  )
+}
