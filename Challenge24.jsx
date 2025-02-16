a5be564b923353c09ffd42faf91a1fe1b509e670
Rechad Hossaini <109422736+RHos1@users.noreply.github.com>
2025-02-16 18:45:16 +0000

    Create Challenge24.jsx

---
+++ Challenge24.jsx
@@ -0,0 +1,19 @@
+import React from "react"
+
+export default function App() {
+    const [messages, setMessages] = React.useState([])
+    /**
+     * Challenge:
+     * - If there are no unread messages, display "You're all caught up!"
+     * - If there's exactly 1 unread message, it should read "You have 
+     *   1 unread message (singular)"
+     * - If there are > 1 unread messages, display "You have <n> unread
+     *   messages" (plural)
+     */
+    return (
+        <div>
+            <h1>{ messages.length > 0 ? `You have ${messages.length}` : "You're all caught up!"}
+            {messages.length === 1 ? " unread message" : " unread messages"}</h1>
+        </div>
+    )
+}
